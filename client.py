b0f2685ed44b14865ac05488f1e4818cf302ab8a
nmercer <nm3rc3r@gmail.com>
2016-06-15 21:43:26 -0700

    Saving

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 import socket
 from settings import *
+import ipgetter
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(PASSWORD, (UDP_IP, UDP_PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((TCP_IP, TCP_PORT))
+sock.send(ipgetter.myip())
+data = sock.recv(BUFFER_SIZE)
+sock.close()
--- server.py
+++ server.py
@@ -1,26 +1,40 @@
 import socket
+import atexit
 from settings import *
 
+def goodbye(conn):
+    conn.close()
+
+def setup_accept(sock):
+    conn, addr = sock.accept()
+    atexit.register(goodbye, conn)
+    return conn
+
 print "Sarting Static IP PI Python Server"
-print "%s:%s" % (UDP_IP, UDP_PORT)
+print "%s:%s" % (TCP_IP, TCP_PORT)
 
 # Todo - Decent readme
 # Todo - Setup Cron
 # Todo - Try Excepts Around This Crap
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((TCP_IP, TCP_PORT))
+sock.listen(1)
+
+conn = setup_accept(sock)
 
 while True:
-    password, addr = sock.recvfrom(1024)
-    if password == PASSWORD:
-        if FILE_PATH:
-            if FILE_PATH[-1] == "/":
-                filename = FILE_PATH + FILE_NAME
-            else:
-                filename = FILE_PATH + "/" + FILE_NAME
+    address = conn.recv(BUFFER_SIZE)
+    print address
+    conn.close()
+    conn = setup_accept(sock)
+    if FILE_PATH:
+        if FILE_PATH[-1] == "/":
+            filename = FILE_PATH + FILE_NAME
         else:
-            filename = FILE_NAME
+            filename = FILE_PATH + "/" + FILE_NAME
+    else:
+        filename = FILE_NAME
 
-        f = open(filename, 'w')
-        f.write(addr[0])
-        f.close()
+    f = open(filename, 'w')
+    f.write(address)
+    f.close()
